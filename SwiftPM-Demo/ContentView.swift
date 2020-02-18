690f37702e6cdd02c12b9d681ffef8c43ac4a05a
Balazs Szamody <bszamody@gmail.com>
2020-02-19 08:21:23 +1100

    Demo Start

---
+++ SwiftPM-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPM-Demo
+//
+//  Created by Balázs Szamódy on 16/2/20.
+//  Copyright © 2020 Balázs Szamódy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
