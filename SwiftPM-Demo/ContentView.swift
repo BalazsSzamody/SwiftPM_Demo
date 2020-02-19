524b7051105273276b585fef3b636c1c8fb19a5b
Balazs Szamody <bszamody@gmail.com>
2020-02-19 22:17:52 +1100

    Textfield with colouring added

--- SwiftPM-Demo/ContentView.swift
+++ SwiftPM-Demo/ContentView.swift
@@ -10,14 +10,21 @@ import SwiftUI
 import PackageA
 import PackageC
 import PackageB
+import StringExtensions
 
 struct ContentView: View {
     let a = PackageA(text: "Time to pack")
+    @State private var email: String = ""
     var body: some View {
         VStack{
             PackageC.CustomText()
             Text(PackageB.text("Cocoaheads"))
             PackageC.CustomText(a)
+            TextField("Email", text: $email)
+                .foregroundColor(email.isEmail ? .green : .red)
+                .textFieldStyle(
+                    RoundedBorderTextFieldStyle())
+                .animation(.default).padding()
         }
     }
 }
